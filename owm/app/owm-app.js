9935abde4571076230f7251986a42551aa57de60
admin <admin@Dev-Interview-MBP.local>
2014-10-03 23:15:25 -0400

    rg

---
+++ owm/app/owm-app.js
@@ -0,0 +1,49 @@
+angular.module('OWMApp', ['ngRoute','ngAnimate'])
+	.value('owmCities', 
+        ['New York', 'Dallas', 'Chicago'])
+    .config(function($routeProvider) {
+        $routeProvider
+	        .when('/', {
+	            templateUrl : './home.html',
+	            controller : 'HomeCtrl'
+	        })
+	        .when('/cities/:city', {
+			    templateUrl : './city.html',
+			    controller : 'CityCtrl',
+			    resolve : {
+			        city: function(owmCities, $route, $location) {
+			            var city = $route.current.params.city;
+			            if(owmCities.indexOf(city) == -1 ) {
+			                $location.path('/error');
+			                return;
+			            }
+			            return city;
+			        }
+			    }
+			})
+			.when('/error', {
+			    template : '<p>Error Page Not Found</p>'
+			})
+			.otherwise({
+		        redirectTo : '/error'
+		    });
+    })
+    .run(function($rootScope, $location, $timeout) {
+	    $rootScope.$on('$routeChangeError', function() {
+	        $location.path('/error');
+	    });
+	    $rootScope.$on('$routeChangeStart', function() {
+	        $rootScope.isLoading = true;
+	    });
+	    $rootScope.$on('$routeChangeSuccess', function() {
+	      $timeout(function() {
+	        $rootScope.isLoading = false;
+	      }, 1000);
+	    });
+	})
+    .controller('HomeCtrl', function($scope) {
+        //empty for now
+    })
+    .controller('CityCtrl', function($scope, city) {
+        $scope.city = city;
+    });
